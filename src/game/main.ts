79c64c04b00009ad629bdefda38ac88ee8989b9a
austrowj <austrowj@users.noreply.github.com>
2026-01-10 17:19:59 -0500

    chore: disable main menu scene

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,7 +1,7 @@
 import { Boot } from './scenes/Boot';
 import { GameOver } from './scenes/GameOver';
 import { Game as MainGame } from './scenes/Game';
-import { MainMenu } from './scenes/MainMenu';
+//import { MainMenu } from './scenes/MainMenu';
 import { AUTO, Game } from 'phaser';
 import { Preloader } from './scenes/Preloader';
 
@@ -22,7 +22,7 @@ const config: Phaser.Types.Core.GameConfig = {
     scene: [
         Boot,
         Preloader,
-        MainMenu,
+        //MainMenu,
         MainGame,
         GameOver
     ]
